7d127d7ec6710385114fb19de6c617bc470045d0
Eduardo Caparros <educaparrosbarcelo@gmail.com>
2023-02-12 23:43:53 +0100

    Priority presentation background

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
+import Image from 'next/image'
 import DefaultHead from '../src/components/DefaultHead'
 import Footer from '../src/containers/footer/Footer'
 import Presentation from '../src/containers/presentation/Presentation'
 import ProjectsSection from '../src/containers/proyectsSection/ProjectsSection'
 import SkillsSection from '../src/containers/skillsSection/SkillsSection'
+import styles from '../styles/Home.module.css'
+import background from '../public/background.jpg'
 
 export default function Home() {
 	return (
 		<>
 			<DefaultHead />
 			<main>
+				<Image
+					src={background}
+					alt='Fondo'
+					className={styles.background}
+					width={background.width}
+					height={background.height}
+					priority
+				/>
 				<Presentation />
 				<SkillsSection />
 				<ProjectsSection />
